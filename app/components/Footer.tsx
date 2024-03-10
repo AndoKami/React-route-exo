bbe97a87e6ece0dde79145abd79774f8682e2ec4
AndoKami <hei.ando.1007@gmail.com>
2024-03-10 23:59:35 +0300

    add: adding header and footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className="w-full bg-black border-t border-t-gray-700 text-center py-5">
+            <h1 className="text-2xl font-bold">
+                Footer
+            </h1>
+        </div>
+    );
+}
